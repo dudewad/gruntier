0c254dc2ce33b70c8c08bda091aeef78dd7dd29f
dudewad <(Ghost117!)>
2014-06-21 14:09:46 -0700

    Updating uglify Grunt task to have proper base directory configuration

--- grunt/taskConfig/uglify.js
+++ grunt/taskConfig/uglify.js
@@ -1,7 +1,7 @@
 module.exports = {
     dist: {
         files: {
-            "<%=pkg.directories.dev_prod %><%=pkg.directories.js %>app.js": "<%=pkg.directories.dev_root %><%=pkg.directories.js %>app.js"
+            "<%=pkg.directories.prod_root %><%=pkg.directories.js %>app.js": "<%=pkg.directories.dev_root %><%=pkg.directories.js %>app.js"
         }
     }
 };
