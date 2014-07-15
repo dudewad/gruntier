2d27dac4a9604baaf5d701d0066ba55212571add
Layton Miller <layton@desmill.com>
2014-07-15 15:22:11 -0700

    Removing some unnecessary files

--- dev/js/globals.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by pdx-win8user1 on 6/17/2014.
- */
-
-var globalVars = [];
-globalVars[0] = 1;
--- prod/js/app.js
+++
@@ -1,1 +0,0 @@
-var globalVars=[];globalVars[0]=1,function(){var a=17;a=18}(),function(){var a=function(a){return a+10};globalVars[0]=a(10)}();
