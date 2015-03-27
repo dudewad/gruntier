d1c517410d68dbddb4442211cbf4b99448cbb9e6
Layton Miller <layton@desmill.com>
2015-03-27 10:21:42 -0700

    Removing app.js as it is a compiled file.

--- dev/js/app.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-    var b = document.getElementsByTagName("body")[0];
-    var p = document.createElement("P");
-    p.innerHTML = "This text was output by our compiled script!";
-    b.appendChild(p);
-};
